dd1525c3cce5d4a331e5c3ba10bf4322a2f7d726
NguyenDuyKhang <nguyenduykhanghuflit@gmail.com>
2023-12-20 22:51:48 +0700

    task management

--- server.js
+++ server.js
@@ -1,42 +1,98 @@
 require('dotenv').config();
 const Telegram = require('node-telegram-bot-api');
-const { Configuration, OpenAIApi } = require('openai');
 
 const express = require('express');
 const app = express();
-
-const config = new Configuration({
-  apiKey: process.env.OPENAI_TOKEN,
+const bot = new Telegram(process.env.BOT_TOKEN, { polling: true });
+// Định nghĩa một route đơn giản
+app.get('/', (req, res) => {
+   res.send('Telegram bot service is running');
 });
 
-const openai = new OpenAIApi(config);
+app.get('/send', (req, res) => {
+   const { msg, chatId } = req.query;
 
-const bot = new Telegram(process.env.BOT_TOKEN, { polling: true });
+   if (!msg || !chatId)
+      return res.status(400).json({ msg: 'msg or chatId invalid' });
+
+   bot.sendMessage(chatId, msg);
+   return res.status(200).json({ msg: 'Send success' });
+});
 
 bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg?.chat?.id, 'Welcome To AI ChatBot');
-  bot.sendMessage(
-    msg?.chat?.id,
-    'Để đặt câu hỏi hãy sử dụng lệnh " /a câu hỏi của bạn "'
-  );
+   bot.sendMessage(
+      msg?.chat?.id,
+      '👻👻👻 Welcome to GoldenLotus Bot, bot này dùng để nhận thông báo về các task bạn được giao'
+   );
+   bot.sendMessage(
+      msg?.chat?.id,
+      '✍️✍️✍️ Để đăng ký nhận thông báo về task vui lòng nhập lệnh sau: /task my_userId'
+   );
+   bot.sendMessage(
+      msg?.chat?.id,
+      '🤳🤳🤳 Để biết được userId của mình là gì vui lòng truy cập https://crm.senvangsolutions.com/getuserid'
+   );
+});
+
+bot.onText(/\/task (.+)/, (msg, match) => {
+   const chatId = msg?.chat?.id;
+   console.log(chatId);
+   try {
+      const userId = match[1];
+      //VẦN VALIDATE LẠI USERID, lưu user ứng chatid vào đb
+      //cần check xem chatid này đã có đăng ký nhận thông báo chưa
+
+      bot.sendMessage(
+         chatId,
+         `🌟🌟🌟 Bạn đã đăng ký nhận thông báo công việc thành công với userId: ${userId}`
+      );
+      bot.sendMessage(
+         chatId,
+         `🔥🔥🔥 Nếu bạn nhập sai userId? Đừng lo lắng hãy nhập lệnh sau:/change new_userId`
+      );
+      bot.sendMessage(
+         chatId,
+         `🧑🏽‍💻🧑🏽‍💻🧑🏽‍💻 Nếu bạn muốn tắt nhận thông báo, hoặc thay đổi userId vui lòng liên hệ admin nhé`
+      );
+   } catch (error) {
+      bot.sendMessage(chatId, 'Bot gặp lỗi rồi, vui lòng thử lại sau nhé!!');
+   }
+});
+
+bot.onText(/\/change (.+)/, (msg, match) => {
+   const chatId = msg?.chat?.id;
+   try {
+      const userId = match[1];
+      //VẦN VALIDATE LẠI USERID, lưu user ứng chatid vào đb
+      //cần check xem chatid đã tồn tại userid này chưa
+
+      bot.sendMessage(
+         chatId,
+         `🌟🌟🌟 Bạn đã thay đổi userId nhận thông báo thành: ${userId}`
+      );
+      bot.sendMessage(
+         chatId,
+         `🔥🔥🔥 Nếu bạn nhập sai userId? Đừng lo lắng hãy nhập lệnh sau:/change new_userId`
+      );
+      bot.sendMessage(
+         chatId,
+         `🧑🏽‍💻🧑🏽‍💻🧑🏽‍💻 Nếu bạn muốn tắt nhận thông báo, hoặc thay đổi userId vui lòng liên hệ admin nhé`
+      );
+   } catch (error) {
+      bot.sendMessage(
+         chatId,
+         '💥💥💥💥 Bot gặp lỗi rồi, vui lòng thử lại sau nhé!!'
+      );
+   }
 });
 
 bot.on('message', async (msg) => {
-  const chatId = msg?.chat?.id;
-
-  if (msg.text?.startsWith('/a ')) {
-    const reply = await openai.createCompletion({
-      max_tokens: 500,
-      model: 'text-davinci-003', //tìm hiểu tại:https://platform.openai.com/docs/models/overview
-      prompt: msg.text,
-      temperature: 0.5,
-    });
-
-    bot.sendMessage(chatId, reply?.data?.choices[0]?.text);
-  }
+   const chatId = msg?.chat?.id;
+   console.log(chatId);
+   try {
+   } catch (error) {
+      console.log(error);
+   }
 });
 
-app.get('/', (req, res) => res.send('oke bot!!'));
-app.listen(3000, () => {
-  console.log('Bot is running in port 3000');
-});
+app.listen(8080, () => console.log('Telegram bot service is running'));
